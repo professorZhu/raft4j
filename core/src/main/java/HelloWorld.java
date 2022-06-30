e21fec41b2eeedd9620fddf7018883713b268ee8
zhuqiang25 <zhuqiang3@jd.com>
2022-06-30 14:59:41 +0800

    初始化

---
+++ core/src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @copyright Copyright 2017-2022 JD.COM All Right Reserved
+ * @author: zhuqiang3
+ * @version:
+ * @Date: 2022-06-30 14:56
+ * @Modified By:
+ * @Description: TODO
+ * @see
+ */
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("hello world");
+    }
+}
